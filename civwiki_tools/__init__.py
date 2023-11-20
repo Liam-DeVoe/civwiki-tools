d4a1c364dfc287ffc03dd65f3b5c32a21bba5cb6
Liam DeVoe <orionldevoe@gmail.com>
2023-11-20 16:28:41 -0500

    log in on init

--- civwiki_tools/__init__.py
+++ civwiki_tools/__init__.py
@@ -1,3 +1,29 @@
+from pathlib import Path
+
+from pywikibot.login import ClientLoginManager
+from pywikibot.config import usernames
+
 from civwiki_tools.utils import site
 
+
 __all__ = ["site"]
+
+mod = {}
+with open(Path(__file__).parent.parent / "config.py") as f:
+    source = f.read()
+
+exec(source, mod)
+
+family_name = site.family.name
+# username is retrieved from pywikibot after it parses user-config.py.
+# password is retrieved separately by us from config.py.
+# pywikibot was not really built to be used as a library...this was the nicest
+# solution I could find that still gave me a reasonable amount of control over
+# when and how logins happen.
+user = usernames[family_name]["en"]
+password = mod["password"]
+
+manager = ClientLoginManager(user=user, password=password, site=site)
+manager.login()
+# force a re-fetch of site information
+del site.userinfo
