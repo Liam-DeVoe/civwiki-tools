5a0fd30ab002f0809c032429599afd0c065ee045
Liam DeVoe <orionldevoe@gmail.com>
2023-11-22 13:39:02 -0500

    add batch processing script

---
+++ batch.py
@@ -0,0 +1,9 @@
+import os
+
+with open("input.txt") as f:
+    lines = f.readlines()
+
+for line in lines:
+    line = line.strip()
+    print(f"processing {line}")
+    os.system(f"python3 scripts/import_item_image.py \"{line}\"")
